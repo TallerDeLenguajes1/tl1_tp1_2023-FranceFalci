accbe6de1764d4454e4d3164f0e8e668c41e88c7
Francesca Falci <francescafacia@gmail.com>
2023-03-16 18:52:07 -0300

    hice el punto e

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,19 @@
 
 int main (){
     printf("hola mundo");
+
+    int numero = 10 ; 
+    int *punt = &numero;
+
+    printf("%d", punt);
+    printf("%d", &numero);
+    printf("%d", &punt);
+
+    printf("%d", *punt );
+    printf("%d", sizeof(numero));
+
+
+    
+
     return 0;
 }
